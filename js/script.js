e7128ac67e1151f86a28f1343bc1e74b8a815fac
Michal Dydo <michal1.dydo@gmail.com>
2016-10-17 23:29:54 +0200

    Edited js and css files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
+var inputElement = document.getElementById('country-name');
 
-$('#search').click(searchCountries);
+$('#country-name').keypress(function (event) {
+	if (event.keyCode == 13) {
+		event.preventDefault();
+		searchCountries();
+		clearText();
+	}
+});
+
+$('#search').click(function () {
+	searchCountries();
+	clearText();
+});
+
+function clearText() {
+	inputElement.value = "";
+}
+
+function mlnFormatter(num) {
+	if (num > 999999) {
+		return (num/1000000).toFixed(1) + ' mln';
+	}
+	if (num > 999) {
+		return (num/1000).toFixed(1) + ' k';
+	}
+}
 
 function searchCountries() {
 	var countryName = $('#country-name').val();
@@ -17,17 +42,32 @@ function showCountriesList(resp) {
 	countriesList.empty();
 	resp.forEach(function (item) {
 		var box = $('<div>').addClass('box');
-		var country = $('<h2>').addClass('country').text(item.name);
+		var country = $('<p>').addClass('country').text(item.name);
 		var capital = $('<p>').addClass('capital').text(item.capital);
 		var currencies = $('<p>').addClass('currencies').text(item.currencies);
-		var population = $('<p>').addClass('population').text(item.population);
+		var population = $('<p>').addClass('population').text(mlnFormatter(item.population));
 		var subregion = $('<p>').addClass('subregion').text(item.subregion);
+		var nativeName = $('<p>').addClass('nativeName').text(item.nativeName);
 
-		box.append(country)
+		var countryBox = $('<p>').addClass('countryBox').text('Country:');
+		var capitalBox = $('<p>').addClass('capitalBox').text('Capital:');
+		var currenciesBox = $('<p>').addClass('currenciesBox').text('Currencies:');
+		var populationBox = $('<p>').addClass('populationBox').text('Population:');
+		var subregionBox = $('<p>').addClass('subregionBox').text('Subregion:');
+		var nativeNameBox = $('<p>').addClass('nativeNameBox').text('Native name:');
+
+		box.append(countryBox)
+			.append(country)
+			.append(capitalBox)
 			.append(capital)
+			.append(currenciesBox)
 			.append(currencies)
+			.append(populationBox)
 			.append(population)
-			.append(subregion);
+			.append(subregionBox)
+			.append(subregion)
+			.append(nativeNameBox)
+			.append(nativeName);
 
 		box.appendTo(countriesList);
 	});
